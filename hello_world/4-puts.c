cfe704f52f4ac1e71f7151eafebd5de7af19dba7
Nihad <7404@holbertonstudents.com>
2023-10-13 04:42:19 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 
-  char mystr[]="Programming is like building a multilingual puzzle";
+  char mystr[]="\"Programming is like building a multilingual puzzle";
   puts(mystr);
   return (0);
 }
