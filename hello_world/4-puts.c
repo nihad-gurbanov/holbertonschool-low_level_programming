812d104d6a2cd5a785aa2625af14545f2392fba8
Nihad <7404@holbertonstudents.com>
2023-10-13 04:58:59 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+char mystr[] = "\"Programming is like building a multilingual puzzle";
 
-  char mystr[]="\"Programming is like building a multilingual puzzle";
-  puts(mystr);
-  return (0);
+puts(mystr);
+return (0);
 }
