53bb2f59869ee6aed42083fff462d48ddfb335c4
Nihad <7404@holbertonstudents.com>
2023-10-15 04:18:38 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message)-1);
-    return 1;
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message  followed by a new line, to standard error.
+ * Return: Always 1.
+ */
+int main(void)
+{
+const char message[] =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, sizeof(message) - 1);
+return (1);
 }
