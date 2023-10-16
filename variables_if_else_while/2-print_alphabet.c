464abac824bf9c91f969ad764b08b2615ad91c16
Nihad <7404@holbertonstudents.com>
2023-10-16 01:13:48 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter_;
+for (letter_ = 'a'; letter_ <= 'z'; letter_++)
+{
+putchar(letter_);
+}
+putchar('\n');
+return (0);
+}
