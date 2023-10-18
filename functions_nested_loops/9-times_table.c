6597679155f0a851c378b9220cb852f0b120fc73
Nihad <7404@holbertonstudents.com>
2023-10-18 06:30:59 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,45 @@
 #include "main.h"
 
+/**
+ * times_table - generates times table
+ * Return: void
+ */
+
 void times_table(void)
 {
 	int i, j;
 	int mult;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			mult = i*j;
-			if (mult>9)
+			mult = i * j;
+
+			if (j > 0 && mult <= 9)
 			{
-			_putchar(mult/10 + '0');
-			_putchar(mult%10 + '0');
-			_putchar(',');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+
+			}
+			else if (j > 0 && mult > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (mult >= 10)
+			{
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
 			}
 			else
 			{
 			_putchar(mult + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 			}
-	
 		}
 		_putchar('\n');
 	}
 }
+
