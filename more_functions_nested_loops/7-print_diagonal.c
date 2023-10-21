bdcd329cb10fb55b966ec3f3944e0b1dcb3567fd
Nihad <7404@holbertonstudents.com>
2023-10-21 06:53:59 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line on the t
+ * @n: n is the number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int width, space;
+	if (n > 0)
+	{
+		for (width = 0; width < n; width++)
+		{
+			for (space = 0; space < width; space++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+
+			if (width == (n - 1))
+			continue;
+			
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
