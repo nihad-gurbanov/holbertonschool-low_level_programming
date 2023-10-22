9d0a09637cf9712a6bbd861ef30c213cddc0425c
Nihad <7404@holbertonstudents.com>
2023-10-21 21:58:34 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints numbers from 1 to 100,
+ * replaces any number:
+ * with "Fizz" if its reminder from division to 3 is 0
+ * with "Buzz" if its reminder from division to 5 is 0
+ * with "FizzBuzz" if its reminder from division to 15 is 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
