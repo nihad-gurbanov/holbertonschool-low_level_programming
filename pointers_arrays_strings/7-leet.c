b5cfabd19db66cf809cebf85ca86431f8f8a84e3
Nihad <7404@holbertonstudents.com>
2023-10-28 11:59:21 -0700

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * leet - is Write a function that encodes a string into 1337
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ *
+ * @str: string argument
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+	}
+	return (str);
+}
+
