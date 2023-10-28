8a40b8243b19123813dc92a9ee3e0f5c40598865
Nihad <7404@holbertonstudents.com>
2023-10-28 10:50:32 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,28 +4,39 @@
  * cap_string- is a function that capitalizes all words of a string
  *
  * @str: string argument
+ * Return: capitalized string
  */
 
 char *cap_string(char *str)
 {
-    int offset = 'a' - 'A';
-    int i;
-    
-    for(i = 0; str[i] != '\0'; i++)
-    {
-	    if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ' '  || str[i - 1] == '\t' || str[i - 1] == '\n'|| str[i - 1] == '.'))
-        {
-            str[i] -= offset;
-        }
-    }
-    i = 0;
-    if (str[i] >= 'a' && str[i] <= 'z')
-    {
-	    str[i] -= offset;
-    }
+	int offset = 'a' - 'A';
+	int i;
 
-    
-     return (str);
-    
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z')
+			       && (str[i - 1] == ' '
+				       || str[i - 1] == '\t'
+				       || str[i - 1] == '\n'
+				       || str[i - 1] == '.'
+				       || str[i - 1] == ','
+				       || str[i - 1] == ';'
+				       || str[i - 1] == '!'
+				       || str[i - 1] == '?'
+				       || str[i - 1] == '"'
+				       || str[i - 1] == '('
+				       || str[i - 1] == ')'
+				       || str[i - 1] == '{'
+				       || str[i - 1] == '}'))
+		{
+			str[i] -= offset;
+		}
+	}
+	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= offset;
+	}
+	return (str);
 }
 
