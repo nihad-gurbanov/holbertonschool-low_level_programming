0789a2fb3b4e57c00534a07459142f7adef6c903
Nihad <7404@holbertonstudents.com>
2023-10-31 07:37:35 -0700

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * sqrt_checker - finds square root if exist
+ *
+ * @x: given number to find square root of it
+ * @y: base case for recursion
+ * Return: square root of given number
+ */
+
+
+int sqrt_checker(int x, int y)
+{
+	if (y * y == x)
+	{
+		return (y);
+	}
+	else if (y * y > x)
+	{
+		return (-1);
+	}
+	else
+	{
+		return(sqrt_checker(x, y + 1));
+	}
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number argument
+ * Return: square root of given number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_checker(n, 0));
+	}
+}
