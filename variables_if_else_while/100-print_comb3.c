eaa2278d089bf8b15732e87ca5eb829193b76a79
Nihad <7404@holbertonstudents.com>
2023-11-02 04:43:26 -0700

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,9 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-			}
+			putchar(i + '0');
+			putchar(j + '0');
+
 			if (i < 8)
 			{
 				putchar(',');
