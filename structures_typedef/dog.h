348e52301599257c11989464fed71731f6f59c3c
Nihad Gurbanov <7404@holbertonstudents.com>
2023-11-21 03:37:40 -0800

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog structure
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+#endif
