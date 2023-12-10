5bbf30b8957c0c2f4a066c99adcfe80227b433b7
Nihad Gurbanov <7404@holbertonstudents.com>
2023-12-10 11:45:41 -0800

    5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int counter = 0;
 	unsigned long int xor_result = n ^ m;
-	
+
 	while (xor_result)
 	{
 		counter += xor_result & 1;
