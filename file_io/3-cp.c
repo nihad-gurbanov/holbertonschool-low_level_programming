0b0cbbf53393c0d05c3a0f4ee9816a7c125804b1
Nihad Gurbanov <7404@holbertonstudents.com>
2023-12-16 11:21:45 -0800

    3-cp.c bug fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "", -1);
+		error_exit(97, "Usage: cp file_from file_to", "", "");
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
