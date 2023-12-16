c608a735aa7aa097fa0da468e7bb92b90b7abfc0
Nihad Gurbanov <7404@holbertonstudents.com>
2023-12-16 12:24:04 -0800

    3-cp.c bug fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,7 @@ void error_exit(int code, const char *message, const char *filename, int fd)
 {
 	if (fd == 99)
 		dprintf(2, "%s %s\n", message, filename);
-	else if (fd == 98)
+	else if (fd == -1)
 		dprintf(2, "%s %s\n", message, filename);
 	else if (fd == 97)
 		dprintf(2, "%s", message);
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file", argv[1], 98);
+		error_exit(98, "Error: Can't read from file", argv[1], -1);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
