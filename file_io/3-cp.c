e3693e9a7e0269a1b56c77896965f6e39105d19a
Nihad Gurbanov <7404@holbertonstudents.com>
2023-12-16 11:27:58 -0800

    3-cp.c bug fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,9 +13,9 @@
  * @filename: Name of the file associated with the error.
  * @fd: File descriptor associated with the error.
  */
-void error_exit(int code, const char *message, const char *filename, int fd)
+void error_exit(int code, const char *message, int fd)
 {
-	dprintf(2, "%s %s %d\n", message, filename, fd);
+	dprintf(2, "%s %d\n", message, fd);
 	exit(code);
 }
 
@@ -34,31 +34,31 @@ int main(int argc, char **argv)
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to", "", "");
+		error_exit(97, "Usage: cp file_from file_to", -1);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file", argv[1], -1);
+		error_exit(98, "Error: Can't read from file", -1);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to file", argv[2], -1);
+		error_exit(99, "Error: Can't write to file", -1);
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to file", argv[2], -1);
+			error_exit(99, "Error: Can't write to file", -1);
 	}
 
 	if (bytes_read == -1)
-		error_exit(98, "Error: Can't read from file", argv[1], -1);
+		error_exit(98, "Error: Can't read from file", -1);
 
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd", argv[1], fd_from);
+		error_exit(100, "Error: Can't close fd", fd_from);
 
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd", argv[2], fd_to);
+		error_exit(100, "Error: Can't close fd", fd_to);
 
 	return (0);
 }
