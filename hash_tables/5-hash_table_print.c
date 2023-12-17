171462ee964644b9ddf5e07faeff0f728ebe9e4e
Nihad Gurbanov <7404@holbertonstudents.com>
2023-12-17 11:03:03 -0800

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ *
+ * @ht: is hash table.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			if (current != NULL)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
+
